1fdda3fa725b9f2469048131c87f049d3a2568e4
hasnae <hassnaenidhaddou@gmail.com>
2024-04-24 11:17:53 +0100

    coomit1

---
+++ kanban/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+    //middleware('auth') ila mknash connecter ma ghykonsh 3dna acces lhad page home
+})->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
